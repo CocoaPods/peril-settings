c81256f359355375315f41d3ef806781ae140978
Orta Therox <orta.therox@gmail.com>
2017-08-19 13:49:17 -0400

    Adds support for checking an issue is the template

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,3 @@
+Hi there, thanks for making an issue. 
+
+There's a lot of info about Peril and our setup in the README, so give those a check and let us know if there's anything else we can do to help.
--- org/issue.ts
+++ org/issue.ts
@@ -1,14 +1,13 @@
-import { danger, schedule } from "danger"
-
+import { danger, schedule, markdown } from "danger"
 
 const gh = danger.github as any // danger/peril#128
 const issue = gh.issue
+const repo = gh.repository
 
 // Support quietly transforming issue bodies from "Cocoapods" to "CocoaPods"
 //
 if (issue.body.includes("Cocoapods")) {
   const newBody = issue.body.replace(/Cocoapods/, "CocoaPods")
-  const repo = gh.repository
 
   schedule(async () => {
     await danger.github.api.issues.edit({
@@ -19,4 +18,33 @@ if (issue.body.includes("Cocoapods")) {
     })
   })
 }
+
+// Support checking if the issue has the same content as the issue template.
+
+schedule( async () => {
+  const repo = gh.repository
+  
+  // danger/peril#129
+  const ghIssueTemplateResponse = await danger.github.api.repos.getContent({
+    owner: repo.owner.login,
+    repo: repo.name,
+    path: ".github/ISSUE_TEMPLATE.md"
+  })
+  const buffer = new Buffer(ghIssueTemplateResponse.content, "base64")
+  const template = buffer.toString()
+
+  if (buffer && buffer === issue.body) {
+    markdown(`
+Hi there, thanks for the issue, but it seem that this issue is just the default template. Please re-create an issue with 
+the template filled out.
+    `)
+
+    await danger.github.api.issues.edit({
+      owner: repo.owner.login,
+      repo: repo.name,
+      number: issue.number,
+      state: "closed"
+    })
+  }
+})
 
