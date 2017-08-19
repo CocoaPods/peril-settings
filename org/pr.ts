ef40525800c83835d617eb91a99b3b731cd0b427
Orta Therox <orta.therox@gmail.com>
2017-08-19 15:08:33 -0400

    Update spell checker

--- org/pr.ts
+++ org/pr.ts
@@ -4,4 +4,4 @@ const anySchedule = schedule as any // danger/danger-js#344
 // Check all markdown documents for spelling issues.
 //
 import spellcheck from 'danger-plugin-spellcheck'
-anySchedule(spellcheck())
+anySchedule(spellcheck({ settings: "CocoaPods/peril-settings@spellcheck.json" }))
